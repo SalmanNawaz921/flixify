9e27fcded313427a85596026e79ca787ad141547
Muhammad <salmannawaz008@gmail.com>
2024-02-10 23:53:53 +0500

    fixed more bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,13 @@
-import { Movie } from "@mui/icons-material";
 import { CssBaseline } from "@mui/material";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Actors, Movies, MovieInfo, Navbar, Profile } from ".";
 import useStyles from "./styles";
 import useAlan from "./Alan";
 import { useEffect, useRef } from "react";
-import { createTheme } from "@mui/system";
 
 const App = () => {
   const alanBtnContainer = useRef(null);
-  const theme = createTheme();
-  // const navigate = useNavigate();
   useAlan();
-
-  // useEffect(() => {
-  //   navigate("/");
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
 
   const classes = useStyles();
   return (
@@ -34,7 +25,6 @@ const App = () => {
           <Route exact path="/actors/:id" element={<Actors />} />
           <Route exact path="/movie/:id" element={<MovieInfo />} />
           <Route exact path="/profile/:id" element={<Profile />} />
-          {/* <Route exact path="*" element={<NoMatch />} /> */}
         </Routes>
       </main>
       <div ref={alanBtnContainer} />
